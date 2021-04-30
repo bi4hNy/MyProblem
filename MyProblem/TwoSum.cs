907798811465d226a5a1e736f79e96585de77611
bi4hNy <qugunren1989@126.com>
2021-04-30 16:12:13 +0800

    添加问题

---
+++ MyProblem/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MyProblem
+{
+    public class Twosum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int o = target - nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (o == nums[j])
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
