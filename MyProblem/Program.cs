dc8c684ecb8cb9aaabc6e09e184850a69d9721ef
bi4hNy <qugunren1989@126.com>
2021-04-30 15:13:30 +0800

    asd

--- MyProblem/Program.cs
+++ MyProblem/Program.cs
@@ -6,7 +6,7 @@ namespace MyProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
