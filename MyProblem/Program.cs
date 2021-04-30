b09f9233fbfa067f62f4ec7f53dd1cecff2f857d
bi4hNy <qugunren1989@126.com>
2021-04-30 15:09:34 +0800

    添加项目文件。

---
+++ MyProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
